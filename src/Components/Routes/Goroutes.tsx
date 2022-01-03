c574359b9a8d06e5befa9a850af3b6d297108487
asainr <96766868+asainr@users.noreply.github.com>
2022-01-03 03:16:33 -0800

    simple
    sdsdsd

--- src/Components/Routes/Goroutes.tsx
+++ src/Components/Routes/Goroutes.tsx
@@ -2,31 +2,17 @@ import Authpost from '../Post/Home/Authpost';
 import Userlist from '../Post/Form/Userlist'
 import Createpost from '../Post/Form/Createpost';
 import Todo from '../Post/Todo/Todo';
-import {BrowserRouter,Route,Switch } from 'react-router-dom';
-import Login from '../Post/Form/Login';
-import PageNot from '../Headers/PageNot';
+import {BrowserRouter,Route } from 'react-router-dom';
 
-const PrivateRoute = (props:any) => {
-  const token= localStorage.getItem('AccessToken');
-  if(token){
-    return <Route exact={true} path={props.path} component={props.component} />
-  }else {
-    return <Login {...props} />
-  }
-}
 
-function Goroutes(props:any) {
+function Goroutes() {
 
     return (
       <BrowserRouter>
-      <Switch>
-         <Route exact={true} path="/" component={Login} />
-         <PrivateRoute  path="/Home" component={Authpost} />
-         <PrivateRoute path="/todo" component={Todo}/>
-         <PrivateRoute  path="/create" component={Createpost}/>
-        <PrivateRoute  path="/user/:id" component={Userlist}/> 
-        <Route component={PageNot}/>
-  </Switch>
+        <Route path="/Home" component={Authpost} />
+        <Route path="/todo" component={Todo} />
+        <Route path="/create" component={Createpost}/>
+        <Route  path="/user/:id" component={Userlist} />
   </BrowserRouter>
     )
   }
